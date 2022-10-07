f7c06e244b73c297aafd6294fbf254900b2c41ee
Juan <juanmarten99@gmail.com>
2022-10-07 19:54:52 -0300

    check constructors

--- minijava-compiler/src/symbolTable/ast/expressions/access/ASTAccessConstructor.java
+++ minijava-compiler/src/symbolTable/ast/expressions/access/ASTAccessConstructor.java
@@ -1,15 +1,22 @@
 package symbolTable.ast.expressions.access;
 
+import errors.SemanticError;
+import errors.SemanticException;
 import lexicalAnalyzer.Token;
+import symbolTable.ST;
+import symbolTable.entities.STClass;
 import symbolTable.types.STType;
+import symbolTable.types.STTypeReference;
 
 public class ASTAccessConstructor implements ASTAccess{
     private final Token tkClassName;
     private ASTChaining astChaining;
+    private boolean endsWithVariable;
 
     public ASTAccessConstructor(Token tkClassName, ASTChaining astChaining) {
         this.tkClassName = tkClassName;
         this.astChaining = astChaining;
+        endsWithVariable = false;
     }
 
     public void setASTChainng(ASTChaining astChaining) {
@@ -18,7 +25,7 @@ public class ASTAccessConstructor implements ASTAccess{
 
     @Override
     public boolean endsWithVariable() {
-        return false;
+        return endsWithVariable;
     }
 
     public void print() {
@@ -29,11 +36,23 @@ public class ASTAccessConstructor implements ASTAccess{
 
     @Override
     public Token getToken() {
-        return null;//TODO implementar
+        if(astChaining == null)
+            return tkClassName;
+        else
+            return astChaining.getToken();
     }
 
     @Override
-    public STType check() {
-        return null;//TODO implementar
+    public STType check() throws SemanticException {
+        STClass stClass = ST.symbolTable.getSTClass(tkClassName.getLexeme());
+        if(stClass == null)
+            throw new SemanticException(new SemanticError(tkClassName, "la clase " + tkClassName.getLexeme() + " no fue declarada"));
+        if(astChaining == null)
+            return new STTypeReference(tkClassName);
+        else{
+            STType chainingType = astChaining.check(new STTypeReference(tkClassName));
+            endsWithVariable = astChaining.endsWithVariable();
+            return chainingType;
+        }
     }
 }
