3c9bb19724236591600f131278f3bbb805446dce
mjuan99 <juanmarten99@gmail.com>
2022-10-14 17:29:03 -0300

    add unary expression test cases

---
+++ minijava-compiler/resources/conErrores/semIIError54.java
@@ -0,0 +1,13 @@
+//[Error:-|9]
+
+class A{
+    public int a1;
+    public boolean a2;
+    void m1(){
+        a1 = -5;
+        a2 = !(+3 == -3);
+        a1 = -"asd";
+    }
+
+    static void main(){}
+}
