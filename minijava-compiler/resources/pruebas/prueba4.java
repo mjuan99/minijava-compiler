1efaf4a399ff94cabc638e275ce599d0df42f3aa
mjuan99 <juanmarten99@gmail.com>
2022-10-29 16:34:35 -0300

    static method call with parameters

---
+++ minijava-compiler/resources/pruebas/prueba4.java
@@ -0,0 +1,15 @@
+//111
+//222
+//333
+
+class A{
+    static void main(){
+        A.print(111);
+        A.print(222);
+        A.print(333);
+    }
+
+    static void print(int i){
+        System.printIln(i);
+    }
+}
