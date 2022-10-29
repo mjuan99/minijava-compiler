0952894b37b9182904689d4ece096ab0127ddb96
mjuan99 <juanmarten99@gmail.com>
2022-10-29 16:33:47 -0300

    update tag manager

--- minijava-compiler/src/codeGenerator/TagManager.java
+++ minijava-compiler/src/codeGenerator/TagManager.java
@@ -1,9 +1,21 @@
 package codeGenerator;
 
+import java.util.HashMap;
+
 public class TagManager {
-    private static long tagNumber = 0;
+    private static final HashMap<String, Integer> tags = new HashMap<>();
 
     public static String getTag(String tagPrefix){
-        return tagPrefix + "$" + tagNumber++;
+        if(tagPrefix == null)
+            tagPrefix = "";
+        Integer tagNumber = tags.get(tagPrefix);
+        if(tagNumber == null){
+            tagNumber = 1;
+            tags.put(tagPrefix, tagNumber);
+        }else{
+            tagNumber++;
+            tags.put(tagPrefix, tagNumber);
+        }
+        return tagPrefix + "$" + tagNumber;
     }
 }
